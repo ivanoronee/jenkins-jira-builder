9bde0bb7b11556cf8b1e688bfc1f4cf6e474f6fb
ivan <ivanoronee@gmail.com>
2014-10-17 23:11:25 +0300

    Added root action and made job excutable when through api endpoint

--- src/main/java/com/frontline/jenkins/PayLoadInterceptorAction.java
+++ src/main/java/com/frontline/jenkins/PayLoadInterceptorAction.java
@@ -10,37 +10,14 @@ import java.util.List;
 
 import jenkins.model.Jenkins;
 
-import org.kohsuke.stapler.HttpResponse;
-import org.kohsuke.stapler.HttpResponses;
-import org.kohsuke.stapler.QueryParameter;
-import org.kohsuke.stapler.WebMethod;
-
 /**
  * @author frontline
  *
  */
-public class PayLoadInterceptorAction {
-
-	/**
-	 * get a request from webHook and parse the jobs to be built.
-	 * 
-	 * @param issueId
-	 *            the id of the issue that triggered the webHook.
-	 * @return
-	 */
-	@WebMethod(name = { "jira-builder-build" })
-	HttpResponse doBuildJob(@QueryParameter String issueId) {
-
-		@SuppressWarnings("rawtypes")
-		Project project = getMatchingJob(issueId);
-		if (project != null) {
-			project.scheduleBuild2(0);
-		}
-		return HttpResponses.plainText("Buld triggered");
-	}
+public class PayLoadInterceptorAction {	
 
 	@SuppressWarnings("rawtypes")
-	public Project getMatchingJob(String issueId) {
+	public static Project getMatchingJob(String issueId) {
 		List<Project> projects = Jenkins.getInstance().getAllItems(
 				Project.class);
 		for (Project project : projects) {
@@ -51,5 +28,5 @@ public class PayLoadInterceptorAction {
 			}
 		}
 		return null;
-	}
+	}	
 }
---
+++ src/main/java/com/frontline/jenkins/Root.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.frontline.jenkins;
+
+import hudson.Extension;
+import hudson.model.RootAction;
+import hudson.model.Project;
+
+import org.kohsuke.stapler.HttpResponse;
+import org.kohsuke.stapler.HttpResponses;
+import org.kohsuke.stapler.QueryParameter;
+
+/**
+ * @author frontline
+ *
+ */
+@Extension
+public class Root implements RootAction {
+
+	public String getIconFileName() {
+		return "gear.png";
+	}
+
+	public String getDisplayName() {
+		return "Jira Builder";
+	}
+
+	public String getUrlName() {
+		return "jira-builder";
+	}
+	
+	public HttpResponse doBuildJob(@QueryParameter String issueId) {
+
+		@SuppressWarnings("rawtypes")
+		Project project = PayLoadInterceptorAction.getMatchingJob(issueId);
+		if (project != null) {
+			project.scheduleBuild2(0);
+		}
+		return HttpResponses.plainText("Buld triggered: "+issueId);
+	}
+}
